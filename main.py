85b1499ad7ecb00ff206cc2fa6a13dcac2970f4f
WOORI KWON <rnjsdnfl62@gmail.com>
2022-08-05 16:05:23 +0900

    topics

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ a
 "# Hello"
 
 topics = [
-    {"id":1, "title":"commit", "view": 100}
-    {"id":2, "title":"push", "view": 200}
+    {"id":1, "title":"commit", "view": 100},
+    {"id":2, "title":"push", "view": 200},
     {"id":3, "title":"pull", "view": 50}
 ]
 
