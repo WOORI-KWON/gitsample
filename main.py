d6d523ba1722921f12f7d93a582046be34b80e7c
WOORI KWON <rnjsdnfl62@gmail.com>
2022-08-05 16:03:22 +0900

    topics

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ from re import A
 a=1
 a
 "# Hello"
+
+topics = [
+    {"id":1, "title":"commit", "view": 100}
+    {"id":2, "title":"push", "view": 200}
+    {"id":3, "title":"pull", "view": 50}
+]
+
+topics
